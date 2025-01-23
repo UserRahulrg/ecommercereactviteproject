4b3484ea841ef802dedc95201e86e4c94db314bc
Rahul <newrahulgautam@gmail.com>
2025-01-23 23:45:33 +0530

    Update25.01.23.01

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,26 +1,31 @@
+import styles from "./Header.module.css"
+
 const Header = ()=>{
 
     return(
-        <html>
-        <head>
-        </head>
-        <body>
-          <header>
-            <div className="header">
+        <html id={styles.html}>
+        <head id={styles.head}>
+        </head >
+        <body id={styles.body}>
+          <header id={styles.header}>
+            <div className="header" id={styles.headerHeader}>
               <div className="headerNav">
-                <nav>
-                  <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Posts</a></li>
-                    <li><a href="">Music</a></li>
-                    <li><a href="">Free EBooks</a></li>
-                    <li><a href="">News</a></li>
-                    <li><a href="">Contact Us</a></li>
-                    <li><a href="">About Us</a></li>
+                <nav id={styles.headerNav}>
+                  <ul id={styles.headerUl}>
+
+                    <li type="none" id={styles.headerLi}><a href="">Home</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">Posts</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">Music</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">Free EBooks</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">News</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">Contact Us</a></li>
+                    <li type="none" id={styles.headerLi}><a href="">About Us</a></li>
                   </ul>
-                  <div className="headerUserDashboard">
-                    <li><a href="">Sign In</a></li>
-                    <li><a href="">Register</a></li>
+                  <div className="headerUserDashboard" id={styles.headerUl}>
+                    <ul>
+                      <li type="none" id={styles.headerLi}><a href="" >Sign In</a></li>
+                      <li type="none" id={styles.headerLi}><a href="">Register</a></li>
+                    </ul>
                   </div>
                 </nav>
               </div>
